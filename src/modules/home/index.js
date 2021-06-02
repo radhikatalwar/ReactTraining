455c868e5024fac426b7b686bce942b761bbe1d4
radhikatalwar <radhika.talwar@appinventiv.com>
2021-06-02 09:55:36 +0530

    Home Component Added

---
+++ src/modules/home/index.js
@@ -0,0 +1,20 @@
+//imports
+//Component declare/define.
+//inside component write HTML Code
+
+import React, { Component } from "react";
+
+export class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>{"Hello From Home"}</h1>
+      </div>
+    );
+  }
+}
+
+
+
+export default Home;
+//export Home;
