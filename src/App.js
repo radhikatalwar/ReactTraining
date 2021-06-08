450c7d1e0d0e3d2f473eaf7e832d15d5423738ce
radhikatalwar <radhika.talwar@appinventiv.com>
2021-06-08 17:48:17 +0530

    assignment4 Flex

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Assignment3 from "./modules/Assignment3";
+// import Assignment3 from "./modules/Assignment3";
 // import Assignment2 from "./modules/Assignment2";
 // import Home from "./modules/home";
 // import Assignment1 from "./modules/Assignment1";
+import Assignment4 from "./modules/Assignment4";
 
 class App extends React.Component {
   render() {
-    return <Assignment3 />;
+    return <Assignment4 />;
   }
 }
 
