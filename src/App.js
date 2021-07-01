131d9f60b4810bdc061327a3800243d2427a4b1b
radhikatalwar <radhika.talwar@appinventiv.com>
2021-07-01 19:20:05 +0530

    assignment12 Cafe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import ContextAPI from "./modules/Assignment11ContextAPI";
+import Assignment12MaterialUI from "./modules/Assignment12MaterialUI";
+// import ContextAPI from "./modules/Assignment11ContextAPI";
 // import { Provider } from "react-redux";
 // import store from "./modules/Assignment10ReduxAPICall/store/store";
 // import APIData from "./modules/Assignment10ReduxAPICall/screens/APIData";
@@ -8,7 +9,8 @@ import ContextAPI from "./modules/Assignment11ContextAPI";
 
 const App = () => {
   return (
-    <ContextAPI />
+    <Assignment12MaterialUI/>
+    // <ContextAPI />
     // <Provider store={store}>
     //   <APIData />
     // </Provider>
