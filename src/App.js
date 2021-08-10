bddf8857ea48de4b52518040e25d53270f33b9c7
radhikatalwar <radhika.talwar@appinventiv.com>
2021-08-10 15:41:50 +0530

    Assignment26 Todo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 // import { BrowserRouter as Router } from "react-router-dom";
 // import { Assignment24DogSearch } from "./modules/Assignment24DogSearch";
-import { Assignment25SelectData } from "./modules/Assignment25SelectData";
-import store from "./modules/Assignment25SelectData/store/store";
+// import { Assignment25SelectData } from "./modules/Assignment25SelectData";
+// import store from "./modules/Assignment25SelectData/store/store";
+import { Assignment26Todo } from "./modules/Assignment26Todo";
+import { Demo } from "./modules/Demo";
 
 const App = () => {
   return (
@@ -11,9 +13,11 @@ const App = () => {
     // <Router>
     //   <Assignment24DogSearch />
     // </Router>
-    <Provider store={store}>
-      <Assignment25SelectData />
-    </Provider>
+    // <Provider store={store}>
+    //   <Assignment25SelectData />
+    // </Provider>
+    <Assignment26Todo />
+    // <Demo />
   );
 };
 
